06ec4fb617249747c7e9fde50c434da2846578bc
Finlay <f.atwell@albynschool.org>
2024-10-03 12:27:47 +0100

    Add files via upload

---
+++ 1001 Task 3.py
@@ -0,0 +1,29 @@
+#3/10/24
+#1001 Task 3
+#Fatwell
+runner_name = ["AS","CM","ZN","OR","FP"]
+runner_num = [2191,1789,1046,3391,2192]
+runner_time = [49.18,51.36,42.58,58.59,64.03]
+money_raised = [312.50,240.75,210.00,278.90,180.00]
+
+def displayNumbers (numbers):
+    for x in range(len(numbers)):
+        print  (numbers[x]," ",end="")
+    return
+
+def findingMin (numbers):
+    min = runner_time[0]
+    position = 0
+    for i in range(5):
+        if runner_time[i] < min:
+            min = runner_time[i]
+            position = i
+    print()
+    print("The lowest number (minimum) in the list is",min,".")
+    return position
+
+displayNumbers(runner_time)
+position = findingMin(runner_time)
+print("The runner who was fastest is:",runner_name[position])
+print("Their runner number was",runner_num[position])
+print("The amount raised for charity was",money_raised[position])
