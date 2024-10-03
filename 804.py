5188078f81b72c4ac6e9ca4108400a46b0fc22c7
Finlay <f.atwell@albynschool.org>
2024-10-03 12:49:51 +0100

    Code update 804.py

--- 804.py
+++ 804.py
@@ -1,42 +1,54 @@
 #804
 #fatwell
 #Step 1
-totalWeight= 0
-ContainerWeights = []
-counter = 0
-for i in range(5):
-    ContainerWeight = int(input("What is the weight of food in the container: "))
-    ContainerWeights.append(ContainerWeight)
-    while ContainerWeight <0 or ContainerWeight > 200:
-        print("Invalid, a single container can only hold up to 200g")
-        ContainerWeight = input("What is the weight of food in the container: ")
-    totalWeight += ContainerWeight
+
+
+def valid_food_weight():
+    totalWeight= int(0)
+    ContainerWeights = []
+    for i in range(5):
+        ContainerWeight = int(input("What is the weight of food in the container: "))
+        ContainerWeights.append(ContainerWeight)
+        while ContainerWeight <0 or ContainerWeight > 200:
+            print("Invalid, a single container can only hold up to 200g")
+            ContainerWeight = int(input("What is the weight of food in the container: "))
+        totalWeight += ContainerWeight
+    return totalWeight, ContainerWeights
     
 #Step 2
-SizeDog = input("What size is your dog?(small, medium or large): ")
+def size_of_dog():
+    SizeDog = input("What size is your dog?(small, medium or large): ")
+    return SizeDog
 
 #Step 3
-if SizeDog == "medium" and totalWeight > 330 and totalWeight < 440:
-    WeightMessage = "The weight of food is suitable for your medium dog"
-
-if SizeDog == "small" and totalWeight > 110 and totalWeight < 140:
-    WeightMessage = "The weight of food is suitable for your small dog"
-
-if SizeDog == "large" and totalWeight > 690 and totalWeight < 900:
-    WeightMessage = "The weight of food is suitable for your large dog"
-
-else:
-    WeightMessage = "This weight of food is not recommended for the size of your dog"
+def ReccomendationMessage(SizeDog, totalWeight):
+    
+    if SizeDog == "medium" and totalWeight > 330 and totalWeight < 440:
+        WeightMessage = "The weight of food is suitable for your medium dog"
+    if SizeDog == "small" and totalWeight > 110 and totalWeight < 140:
+        WeightMessage = "The weight of food is suitable for your small dog"
+    if SizeDog == "large" and totalWeight > 690 and totalWeight < 900:
+        WeightMessage = "The weight of food is suitable for your large dog"
+    else:
+        WeightMessage = "This weight of food is not recommended for the size of your dog"
+    return(WeightMessage)
 
 #Step 4
-AverageWeight = totalWeight/ 5
-round(AverageWeight, 1)
+def Average_Weight(totalWeight):
+    AverageWeight = totalWeight/ 5
+    return(round(AverageWeight, 1))
 
 #Step 5
-for i in range(5):
-    print(ContainerWeights[counter])
-    counter += 1
-print(totalWeight)
-print(AverageWeight)
-print(WeightMessage)
+def OutputMessages(totalWeight, AverageWeight, WeightMessage):
+    for i in range(5):
+        print(ContainerWeights[i])
+    print(totalWeight)
+    print(AverageWeight)
+    print(WeightMessage)
 
+# main program
+totalWeight, ContainerWeights = valid_food_weight()
+SizeDog = size_of_dog()
+WeightMessage = ReccomendationMessage(SizeDog, totalWeight)
+AverageWeight = Average_Weight(totalWeight)
+OutputMessages(totalWeight, AverageWeight, WeightMessage)
